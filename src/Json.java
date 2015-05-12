ebe5edbc6d1d6866fe91111d75dd8fe3bb294573
Victor Miguez <victorcineis@gmail.com>
2015-05-12 20:52:33 -0300

    lista de carros

--- src/Json.java
+++ src/Json.java
@@ -17,8 +17,24 @@ public class Json {
 	public ArrayList <Carro> listaDeCarros = new ArrayList<Carro>();
 		
 	Carro uno   = new Compacto("Uno", "Fiat", "Trabalho", "Flex", false);
-	Carro gol   = new Compacto("Gol", "Volkswagen", "CASAIS","Flex", false);
-	Carro agile = new Compacto("Agile", "Chevrolet", "Jovens", "Flex", false);
+	Carro gol   = new Compacto("VW Gol", "Ford Ka", "CASAIS","Flex", false);
+	Carro palio = new Compacto("Fiat Palio", "Chevrolet Agile", "Jovens", "Flex", false);
+	
+	Carro montana  = new PickUp("Montana", "Chevrolet", "Flex", false);
+	Carro ranger   = new PickUp("Ranger", "Ford", "CASAIS","Flex", false);
+	Carro hilux    = new PickUp("Hilux", "Toyota", "Jovens", "Flex", false);
+	
+	Carro fusion = new Sedan("Fusion", "Ford", "Flex", false);
+	Carro a6     = new Sedan("A6", "Audi", "CASAIS","Flex", false);
+	Carro accord = new Sedan("Accord", "Honda", "Jovens", "Flex", false);
+	
+	Carro ecosport   = new Suv("Ecosport","Ford", "Flex", false);
+	Carro santa_fe   = new Suv("Santa Fe", "Hyundai", "CASAIS","Flex", false);
+	Carro palio      = new Suv("Fiat Palio", "Chevrolet Agile", "Jovens", "Flex", false);
+	
+	Carro focus   = new Esportivo("Focus", "Ford" , "Flex", false);
+	Carro sandero = new Esportivo("Sandero", "Renault", "CASAIS","Flex", false);
+	Carro astra   = new Esportivo("Astra", "Chevrolet", "Jovens", "Flex", false);
 
 	public void montarListaDeCarros() {
 		listaDeCarros.add(uno);
